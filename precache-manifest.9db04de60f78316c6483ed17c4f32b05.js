8ba3338399f74fd754feed2ae125893c2fef2657
NovaFrime <holyprime1985@gmail.com>
2021-10-15 02:47:34 +0700

    Updates

---
+++ precache-manifest.9db04de60f78316c6483ed17c4f32b05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e216079190b217e81c79236b581150",
+    "url": "/detectsymtom/index.html"
+  },
+  {
+    "revision": "cac34089c99acc94a26a",
+    "url": "/detectsymtom/static/css/main.c45b2298.chunk.css"
+  },
+  {
+    "revision": "72a42049fbfde9db2148",
+    "url": "/detectsymtom/static/js/2.41912ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/detectsymtom/static/js/2.41912ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac34089c99acc94a26a",
+    "url": "/detectsymtom/static/js/main.1b579444.chunk.js"
+  },
+  {
+    "revision": "86dca4b903494d1e0e46",
+    "url": "/detectsymtom/static/js/runtime-main.2d5ffffd.js"
+  },
+  {
+    "revision": "7c583c79e665fe726210608b348137e1",
+    "url": "/detectsymtom/static/media/logo.7c583c79.svg"
+  }
+]);
